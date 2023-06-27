cc9b32e737204168fb357bab7195a93b0786899d
Nate Argaw <59675817+nargaw@users.noreply.github.com>
2023-06-27 06:45:06 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  	import { onMount } from "svelte";
+	import { createScene } from './scene.js'
+
+	console.log(onMount)
+
+	let el
+
+	onMount(() => {
+		createScene(el)
+	})
+	
+</script>
+
+<canvas bind:this={el}></canvas>
+
